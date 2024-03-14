66316b301524df97159ba6dada18964c0649631e
abdulhamit-dev <hmd62392@gmail.com>
2024-03-14 23:16:50 +0300

    global exception

--- Core.Packages/Core.CrossCuttingConcerns/Exceptions/Types/BusinessException.cs
+++ Core.Packages/Core.CrossCuttingConcerns/Exceptions/Types/BusinessException.cs
@@ -4,20 +4,19 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Core.CrossCuttingConcerns.Exceptions.Types
+namespace Core.CrossCuttingConcerns.Exceptions.Types;
+
+public class BusinessException:Exception
 {
-    public class BusinessException:Exception
+    public BusinessException()
     {
-        public BusinessException()
-        {
-            
-        }
-
-        public BusinessException(string? message):base(message) 
-        {
-            
-        }
-
         
     }
+
+    public BusinessException(string? message):base(message) 
+    {
+        
+    }
+
+    
 }
