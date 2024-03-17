305a9ff8c77e848c472bc5862b85e21f70dbe97c
abdulhamit-dev <hmd62392@gmail.com>
2024-03-18 01:27:34 +0300

    file and db logger behavior

---
+++ Core.Packages/Core.CrossCuttingConcerns/Serilog/Logger/PostgreSqlLogger.cs
@@ -0,0 +1,26 @@
+﻿using Core.CrossCuttingConcerns.Serilog.ConfigurationModels;
+using Core.CrossCuttingConcerns.Serilog.Messages;
+using Microsoft.Extensions.Configuration;
+using Serilog;
+using Serilog.Sinks.PostgreSQL;
+
+namespace Core.CrossCuttingConcerns.Serilog.Logger;
+
+public class PostgreSqlLogger:LoggerServiceBase
+{
+    public PostgreSqlLogger(IConfiguration configuration)
+    {
+        PostgreSqlConfiguration logConfiguration =
+            configuration.GetSection("SeriLogConfigurations:PostgreSqlConfiguration").Get<PostgreSqlConfiguration>()
+            ?? throw new Exception(SerilogMessages.NullOptionsMessage);
+
+       
+
+        ColumnOptions columnOptions = new();
+
+        global::Serilog.Core.Logger seriLogConfig = new LoggerConfiguration().WriteTo.PostgreSQL(logConfiguration.ConnectionString,logConfiguration.TableName)
+            .CreateLogger();
+
+        Logger = seriLogConfig;
+    }
+}
